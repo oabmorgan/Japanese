2be28bf3f51a3a725813e5483c7336e3addf7f02
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-05-08 15:50:22 +0900

    fommit

--- scripts.js
+++ scripts.js
@@ -1,31 +1,20 @@
-var teform = [[['する', 'して'],['くる','きて']], //g3
-              [['いる','て'],['える','て']], //g2
-              [['う','って'],['つ','って'],['る','って'],['いく','いって'],['め','んで'],['ぶ','んで'],['む','んで'],['く','いて'],['ぐ','いで'],['す','して']]] //g1
+var grammar = document.getElementById("Grammar");
+var example = document.getElementById("Example");
+var english = document.getElementById("English");
+var definition = document.getElementById("Definition");
 
-function ConjucationTe(){
-    var verb = document.getElementById("verb").value;
-    for(var i=0; i<teform[0].length; i++){
-        var ending = verb.substr(verb.length - teform[0][i][0].length);
-        if(ending == teform[0][i][0]){
-            verb = verb.slice(0, -teform[0][i][0].length);
-            verb += teform[0][i][1];
-        }
-    }
-    
-    for(i=0; i<teform[1].length; i++){
-        ending = verb.substr(verb.length - teform[1][i][0].length);
-        if(ending == teform[1][i][0]){
-            verb = verb.slice(0, -teform[1][i][0].length);
-            verb += teform[1][i][1];
-        }
-    }
-    
-    for(i=2; i<teform[2].length; i++){
-        ending = verb.substr(verb.length - teform[2][i][0].length);
-        if(ending == teform[2][i][0]){
-            verb = verb.slice(0, -teform[2][i][0].length);
-            verb += teform[2][i][1];
-        }
-    }
-    document.getElementById("output").innerHTML = verb;
-}
+grammar.onclick = function() {
+    this.style.height = "auto";
+};
+
+example.onclick = function() {
+    this.style.height = "auto";
+};
+
+english.onclick = function() {
+    this.style.height = "auto";
+};
+
+definition.onclick = function() {
+    this.style.height = "auto";
+};
