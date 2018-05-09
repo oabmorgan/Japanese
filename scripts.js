ced509eeec9db55163aa54af92686ad33c7354cf
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-05-09 12:40:04 +0900

    test

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
 var grammar = document.getElementById("Grammar");
 var example = document.getElementById("Example");
 var english = document.getElementById("English");
@@ -18,3 +20,14 @@ english.onclick = function() {
 definition.onclick = function() {
     document.getElementById("definitionText").style.visibility = 'visible';
 };
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Title").innerHTML = "pass";
+    }
+  };
+
+xhttp.open("GET", "content.xml", true);
+xhttp.send();
