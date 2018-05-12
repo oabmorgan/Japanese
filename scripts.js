63c8b61e1d5eb93b1e5017acc6bdd02870e8df24
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-05-12 15:59:50 +0900

    czx

--- scripts.js
+++ scripts.js
@@ -17,12 +17,12 @@ function ShowAll(){
 function Newcard(cardID){
     var xmlDoc = xml.responseXML;
     var card = xmlDoc.getElementsByTagName('card')[0];
-    document.getElementById("Grammar").innerHTML = card.getElementsByTagName('grammar')[0];
-    document.getElementById("Meaning").innerHTML =  card.getElementsByTagName('meaning')[0];
-    document.getElementById("ExampleJP").innerHTML =  card.getElementsByTagName('example')[0];
-    document.getElementById("ExampleEN").innerHTML =  card.getElementsByTagName('exmeaning')[0];
-    document.getElementById("PageNumber").innerHTML = card.getElementsByTagName('page')[0];
-    document.getElementById("Notes").innerHTML = card.getElementsByTagName('notes')[0];
+    document.getElementById("Grammar").innerHTML = card.getAttribute('grammar');
+    document.getElementById("Meaning").innerHTML =  card.getAttribute('meaning');
+    document.getElementById("ExampleJP").innerHTML =  card.getAttribute('exampleJP');
+    document.getElementById("ExampleEN").innerHTML =  card.getAttribute('exampleEN');
+    document.getElementById("PageNumber").innerHTML = card.getAttribute('grammar');
+    document.getElementById("Notes").innerHTML = card.getAttribute('notes');
     
     //HideAll();
     
