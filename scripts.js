96f6edc37cab368de3186e4597e72bd419dbb4d1
oabmorgan <38126042+oabmorgan@users.noreply.github.com>
2018-05-12 15:43:15 +0900

    new format

--- scripts.js
+++ scripts.js
@@ -1,75 +1,34 @@
-var grammar = document.getElementById("Grammar");
-var example = document.getElementById("Example");
-var english = document.getElementById("English");
-var notes = document.getElementById("Notes");
-var button = document.getElementById("Button");
-var cardID = 0;
-example.onclick = function() {
-    document.getElementById("ExampleText").style.visibility = 'visible';
-};
-
-english.onclick = function() {
-    document.getElementById("EnglishText").style.visibility = 'visible';
-};
-
-notes.onclick = function() {
-    document.getElementById("NotesText").style.visibility = 'visible';
-};
-
 var xml = new XMLHttpRequest();
+xml.open("GET", "content.xml", true);
+xml.send();
+
+function HideAll(){
+    document.getElementById("Meaning").style.display = "none";
+    document.getElementById("ExampleEN").style.display = "none";
+    document.getElementById("Notes").style.display = "none";
+}
+
+function ShowAll(){
+    document.getElementById("Meaning").style.display = "block";
+    document.getElementById("ExampleEN").style.display = "block";
+    document.getElementById("Notes").style.display = "block";
+}
+
+function Newcard(cardID){
+    var xmlDoc = xml.responseXML;
+    var card = xmlDoc.getElementsByTagName('card')[0];
+    document.getElementById("Grammar").innerHTML = card.getElementsByTagName('grammar')[0];
+    document.getElementById("Meaning").innerHTML =  card.getAttribute('meaning');
+    document.getElementById("ExampleJP").innerHTML =  card.getAttribute('example');
+    document.getElementById("ExampleEN").innerHTML =  card.getAttribute('exmeaning');
+    document.getElementById("PageNumber").innerHTML = card.getAttribute('page');
+    document.getElementById("Notes").innerHTML = card.getAttribute('notes');
+    
+    HideAll();
+}
 
 xml.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        LoadCard(0);
+        Newcard(0);
     }
 };
-
-function NextCard(Random){
-    if(Random){
-        RandomCard();
-    } else{
-        cardID ++;
-    }
-    LoadCard();
-}
-
-function RandomCard(){
-    var xmlDoc = xml.responseXML;
-        var cardCount = xmlDoc.getElementsByTagName("card").length;
-        cardID = Math.floor(Math.random() * (cardCount-1));
-}
-
-function LoadCard(){
-    var xmlDoc = xml.responseXML;
-        var grammar = xmlDoc.getElementsByTagName("grammar")[cardID].childNodes[0].nodeValue;
-        var example = xmlDoc.getElementsByTagName("example")[cardID].childNodes[0].nodeValue;
-        var exampleAnswer = xmlDoc.getElementsByTagName("exampleAnswer")[cardID].childNodes[0].nodeValue;
-        var english = xmlDoc.getElementsByTagName("english")[cardID].childNodes[0].nodeValue;
-        var notes = xmlDoc.getElementsByTagName("notes")[cardID].childNodes[0].nodeValue;
-    
-        var level = xmlDoc.getElementsByTagName("level")[cardID].childNodes[0].nodeValue;
-        
-        switch(level){
-                case "N3":
-                document.getElementById("Grammar").style.backgroundColor = 'yellow';
-            case "N4":
-                document.getElementById("Grammar").style.backgroundColor = 'DodgerBlue';
-                break;
-            default:
-                document.getElementById("Grammar").style.backgroundColor = 'lightgreen';
-        }
-        
-        
-        document.getElementById("GrammarTitle").innerHTML = grammar;
-        document.getElementById("ExampleTitle").innerHTML = example;
-        document.getElementById("ExampleText").innerHTML = exampleAnswer;
-        document.getElementById("EnglishText").innerHTML = english;
-        document.getElementById("NotesText").innerHTML = notes;
-    
-    document.getElementById("ExampleText").style.visibility = 'hidden';
-    document.getElementById("EnglishText").style.visibility = 'hidden';
-    document.getElementById("NotesText").style.visibility = 'hidden';
-}
-
-xml.open("GET", "content.xml", true);
-xml.send();
